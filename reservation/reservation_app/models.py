5858f98af94094e8cf66ecdb93b2ed6df7ba17f3
KrzysztofCalus <krzysztofcalus@gmail.com>
2021-06-14 19:03:47 +0200

    feature/room-model

--- reservation/reservation_app/models.py
+++ reservation/reservation_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class ConferenceRoom(models.Model):
+    name = models.CharField(max_length=128)
+    capacity = models.PositiveIntegerField()
+    projector_availability = models.BooleanField(default=False)
